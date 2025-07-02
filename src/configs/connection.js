6566af9b09f35f6c511c216e3b49970f58a6f269
Xaydarovdev <bekzodeoke@gmail.com>
2025-07-02 23:35:17 +0500

    changed code

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -3,10 +3,7 @@ const { connect } = require("mongoose");
 const ConnectToMongodb = async () => {
   try {
     const url = process.env.MONGO_URL || "";
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await connect(url);
     console.log("mongodb is connected");
   } catch (error) {
     console.error("mongoose connection error", error.message);
