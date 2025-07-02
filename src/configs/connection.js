8064d8c1b51686d81f626fdf1915702f7d7f83e9
Xaydarovdev <bekzodeoke@gmail.com>
2025-07-02 23:49:20 +0500

    changed code:

--- src/configs/connection.js
+++ src/configs/connection.js
@@ -3,7 +3,9 @@ const { connect } = require("mongoose");
 const ConnectToMongodb = async () => {
   try {
     const url = process.env.MONGO_URL || "";
-    await connect(url);
+    await connect(url, {
+      connectTimeoutMS: 30000,
+    });
     console.log("mongodb is connected");
   } catch (error) {
     console.error("mongoose connection error", error.message);
